01195ee9a057ffd2a4dcf67a1c217fc007150b8a
PintuBeast <62881486+PintuBeast@users.noreply.github.com>
2021-03-17 16:05:09 +0530

    Update run_mod_old.py

--- run_mod_old.py
+++ run_mod_old.py
@@ -39,170 +39,19 @@ if __name__ == '__main__':
         e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
     else:
         e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
-    
-    os.system('rm -r /openPose/images')
-
-    try: 
-      os.mkdir('/openPose/images') 
-    except OSError as error: 
-      print(error)   
-
-   #video 1 split into frames
-
-    src = cv2.VideoCapture('/openPose/videos/input1.mp4')
-    fps = src.get(cv2.CAP_PROP_FPS)
-
-    frame_num = 0
-    while(frame_num< int(src.get(cv2.CAP_PROP_FRAME_COUNT))):
-      # Capture frame-by-frame
-      ret, frame = src.read()
-
-      # Saves image of the current frame in jpg file
-      name = '/openPose/images/f1rame_' + str(frame_num) + '.png'
-      print ('Creating...' + name)
-      cv2.imwrite(name, frame)
-
-      # To stop duplicate images
-      frame_num += 1
-
-      # When everything done, release the capture
-    src.release()
-    cv2.destroyAllWindows()
-
-
-
-   #video 2 split into frames
-
-    src = cv2.VideoCapture('/openPose/videos/input2.mp4')
-    fps = src.get(cv2.CAP_PROP_FPS)
-
-    frame_num = 0
-    while(frame_num< int(src.get(cv2.CAP_PROP_FRAME_COUNT))):
-      # Capture frame-by-frame
-      ret, frame = src.read()
-
-      # Saves image of the current frame in jpg file
-      name = '/openPose/images/f2rame_' + str(frame_num) + '.png'
-      print ('Creating...' + name)
-      cv2.imwrite(name, frame)
-
-      # To stop duplicate images
-      frame_num += 1
-
-      # When everything done, release the capture
-    src.release()
-    cv2.destroyAllWindows()
-
-
-    f1Count = len(glob.glob1('/openPose/images',"f1rame_*.png"))
-    f2Count = len(glob.glob1('/openPose/images',"f2rame_*.png"))
-
-    # processing first video
-    data1 = {}
-    data1['parts'] = []
-
-    data = {}
-    data['frames'] = []
-
-    for i in range(1,f1Count): 
+   
+    for filename in os.listdir('./images'):
+      file1=os.path.join('./images',filename)
       # estimate human poses from a single image !
-      image = common.read_imgfile('/openPose/images/f1rame_'+str(i)+'.png', None, None)
+      image = common.read_imgfile(file1, None, None)
       if image is None:
           logger.error('Image can not be read, path=%s' % args.imagePath)
           sys.exit(-1)
       t = time.time()
       humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
       elapsed = time.time() - t
-      data1['parts'] = []
-      for human in humans:
-            # draw point
-            for ii in range(common.CocoPart.Background.value):
-                if ii not in human.body_parts.keys():
-                    continue
 
-                body_part = human.body_parts[ii]
-               # center = (int(body_part.x * image_w + 0.5), int(body_part.y * image_h + 0.5))
-                
-                
-                data1['parts'].append({
-                'id': ii,
-                'x': body_part.x,
-                'y': body_part.y
-                })
-            break    
-
-      logger.info('inference image f1rame_: %s in %.4f seconds.' % (str(i), elapsed))
+      logger.info('inference image %.4f seconds.' % ( elapsed))
 
       image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
-      cv2.imwrite('/openPose/output/f1rame_'+str(i)+'.png',cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-     
-      data['frames'].append({
-        'num': i,
-        'array':data1}
-      )
-
-    with open('/openPose/output/data1.json', 'w') as outfile:
-      json.dump(data, outfile)
-
-
-    # processing second video
-    data1 = {}
-    data1['parts'] = []
-
-    data = {}
-    data['frames'] = []
-
-    for i in range(1,f2Count): 
-      # estimate human poses from a single image !
-      image = common.read_imgfile('/openPose/images/f2rame_'+str(i)+'.png', None, None)
-      if image is None:
-          logger.error('Image can not be read, path=%s' % args.imagePath)
-          sys.exit(-1)
-      t = time.time()
-      humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
-      elapsed = time.time() - t
-      data1['parts'] = []
-      for human in humans:
-            # draw point
-            for ii in range(common.CocoPart.Background.value):
-                if ii not in human.body_parts.keys():
-                    continue
-
-                body_part = human.body_parts[ii]
-               # center = (int(body_part.x * image_w + 0.5), int(body_part.y * image_h + 0.5))
-                
-                
-                data1['parts'].append({
-                'id': ii,
-                'x': body_part.x,
-                'y': body_part.y
-                })
-            break    
-
-      logger.info('inference image f2rame_: %s in %.4f seconds.' % (str(i), elapsed))
-
-      image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
-      cv2.imwrite('/openPose/output/f2rame_'+str(i)+'.png',cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-     
-      data['frames'].append({
-        'num': i,
-        'array':data1}
-      )
-
-    with open('/openPose/output/data2.json', 'w') as outfile:
-      json.dump(data, outfile)  
-
-    #os.system('ffmpeg -i /openPose/output/f1rame_%d.png -y -start_number 1 -vf scale=400:800 -c:v libx264 -pix_fmt yuv420p /openPose/output/out1.mp4')
-    #os.system('ffmpeg -i /openPose/output/f2rame_%d.png -y -start_number 1 -vf scale=400:800 -c:v libx264 -pix_fmt yuv420p /openPose/output/out2.mp4')
-
-    fCount = f1Count if f1Count < f2Count else f2Count 
       
-    for i in range(1,290):
-      s_img =cv2.resize(cv2.imread('/openPose/output/f1rame_'+str(i)+'.png'),(270,480))
-      l_img = cv2.resize(cv2.imread('/openPose/output/f2rame_'+str(i)+'.png'),(1080,1920) )
-      x_offset=y_offset=0
-      l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
-      cv2.imwrite('/openPose/output/combo_'+str(i)+'.png',cv2.cvtColor(l_img, cv2.COLOR_BGR2RGB))
-
-    os.system('ffmpeg -i /openPose/output/combo_%d.png -y -start_number 1 -vf scale=400:800 -c:v libx264 -pix_fmt yuv420p /openPose/output/out.mp4')
-  
