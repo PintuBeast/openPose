8cb8970303105a281a94823adaaa510ef0ab63e0
PintuBeast <62881486+PintuBeast@users.noreply.github.com>
2021-04-16 16:56:05 +0530

    Update run_mod.py

--- run_mod.py
+++ run_mod.py
@@ -373,7 +373,7 @@ if __name__ == '__main__':
     #print('frame:1:',f1Count,'frame 2:',f2Count)
     minFrames=f1Count if f1Count < f2Count else f2Count
     if minFrames <1:
-      minFrames=1000
+      minFrames=1
     x1Array=np.zeros((18,minFrames))
     y1Array=np.zeros((18,minFrames))
     x2Array=np.zeros((18,minFrames))
@@ -717,7 +717,8 @@ if __name__ == '__main__':
       x_offset=y_offset=0
       l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
       cv2.putText(l_img, 'Score', (l_img.shape[1]-200,100), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
-      cv2.putText(l_img, str(simArr[i]), (l_img.shape[1]-200,200), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+      if len(simArr)>=fCount:
+        cv2.putText(l_img, str(simArr[i]), (l_img.shape[1]-200,200), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
       cv2.imwrite('/openPose/output_'+args.postID+'/combo_'+str(i)+'.png',l_img)
     os.system('ffmpeg -i /openPose/output_'+args.postID+'/combo_%d.png -y -start_number 1 -c:v libx264 -pix_fmt yuv420p -y /openPose/output_'+args.postID+'/output_main.mp4')
 
