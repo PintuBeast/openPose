696b84bc39fc36d1565f3f06101b940e8c1ea16c
PintuBeast <62881486+PintuBeast@users.noreply.github.com>
2021-07-16 12:49:49 +0530

    Update run_mod.py

--- run_mod.py
+++ run_mod.py
@@ -761,7 +761,7 @@ if __name__ == '__main__':
       l_img = cv2.resize(cv2.imread('/openPose/images_'+args.postID+'/f2rame_'+str(i)+'.png'),(big_w,big_h) )
       x_offset=y_offset=0
       l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
-      cv2.putText(l_img, 'Score', (l_img.shape[1]-200,100), font, 2, (255,0,0), 2, cv2.LINE_AA)
+      cv2.putText(l_img, 'Score', (l_img.shape[1]-200,100), font, 2, (0,0,255), 2, cv2.LINE_AA)
       if len(simArr)>=fCount:
         cv2.putText(l_img, str(round(simArr[i],1)), (l_img.shape[1]-200,200), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
       cv2.imwrite('/openPose/output_'+args.postID+'/combo_'+str(i)+'.png',l_img)
